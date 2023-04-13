430cfef9e4a51209046ee4c0cb5373cf7ca78d65
sumon paul <sumpaul100@gmail.com>
2023-04-13 20:02:02 +0600

    mobile device menu seted need to manage state

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,13 +15,16 @@ const Navbar = () => {
       <div>
         <AiOutlineMenu size={25} />
       </div>
-      <ul className="fixed top-0 left-0 w-[60%] border-r border-r-gray-800 h-full">
-        <li className="p-4">Home</li>
-        <li className="p-4">Company</li>
-        <li className="p-4">Resources</li>
-        <li className="p-4">About</li>
-        <li className="p-4">Contact</li>
-      </ul>
+      <div className="fixed top-0 left-0 w-[60%] border-r border-r-gray-800 bg-[#000300] h-full">
+        <h1 className="uppercase text-3xl text-[#00df9a] font-bold px-4">React.</h1>
+        <ul className="uppercase p-4">
+          <li className="p-4 border-b border-gray-700">Home</li>
+          <li className="p-4 border-b border-gray-700">Company</li>
+          <li className="p-4 border-b border-gray-700">Resources</li>
+          <li className="p-4 border-b border-gray-700">About</li>
+          <li className="p-4">Contact</li>
+        </ul>
+      </div>
     </div>
   );
 };
