2ac2542a84072a6e679b6c22ec6394b5b6454a2a
sumon paul <sumpaul100@gmail.com>
2023-04-16 00:01:16 +0600

    analytics section done

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import laptop from "../assets/laptop.jpg";
 
 const Analytics = () => {
-  return <div className="w-full bg-white py-16">Analytics</div>;
+  return (
+    <div className="w-full bg-white py-16">
+      <div className="max-w-[1224px] mx-auto grid md:grid-cols-2">
+        <div className="">
+          <img className="w-full my-4 mx-auto" src={laptop} alt="laptop" />
+        </div>
+        <div className="flex flex-col justify-center p-4 md:p-0">
+          <p className="uppercase text-[#00df9a] font-bold">Data analytics dashboard</p>
+          <h2 className="capitalize text-2xl sm:text-3xl md:text-4xl font-bold py-2">Manage data analytics centerlly</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum deserunt maiores perferendis ipsa earum enim! Libero error tempora esse maiores at nobis
+            ratione fuga? Laborum vitae nulla odit aperiam blanditiis.
+          </p>
+          <button className="w-[200px] bg-black text-[#00df9a] font-medium py-3 rounded-md mx-auto my-6 text-xl md:text-2xl">Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Analytics;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
         <h1 className="capitalize text-4xl sm:text-6xl md:text-7xl font-bold md:py-6">grow this data.</h1>
         <div className="flex justify-center items-center">
           <p className="text-xl sm:text-4xl md:text-5xl font-boldnpm">Fast, flexible financing for : </p>
-          <TypeAnimation sequence={["BTC", "KTL", "BTF", "BTL", "TCL"]} className="text-xl sm:text-4xl md:text-5xl font-boldnpm ml-1" repeat={Infinity} speed={500} />
+          <TypeAnimation sequence={["BTC", "KTL", "BTF", "BTL", "TCL"]} className="text-xl sm:text-4xl md:text-5xl font-boldnpm ml-2" repeat={Infinity} speed={500} />
         </div>
         <p className="text-xl md:text-2xl font-bold text-gray-600 my-4">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga officiis aliquam pariatur quasi?
