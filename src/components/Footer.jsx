b95276f156bb3982dcd8c6233197936f63d0c721
sumon paul <sumpaul100@gmail.com>
2023-04-17 00:30:32 +0600

    footer start

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="">
+      <div className="max-w-[1240px] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300"></div>
+    </footer>
+  );
+};
+
+export default Footer;
